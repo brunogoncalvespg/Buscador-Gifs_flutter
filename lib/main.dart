670c70792ef59facbf208b1816786d96912b27e4
Bruno Goncalves <brunogoncalvespg@icloud.com>
2021-08-28 00:58:59 -0300

    Commit App

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:buscadordegifs/ui/GifPage.dart';
+import 'package:buscadordegifs/ui/HomePage.dart';
+import 'package:flutter/material.dart';
+import 'package:buscadordegifs/ui/GifPage.dart';
+
+void main() {
+  runApp( MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
